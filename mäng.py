a6fafd03f8aaa03df7cef4387cd7dde48188c843
kerlitasso <kerlitasso@gmail.com>
2022-11-27 11:53:33 +0200

    nupud olid üksteise peal, vajutas korraga kahte

--- mäng.py
+++ mäng.py
@@ -14,7 +14,7 @@ pygame.font.init()
 pygame.display.set_caption("Are you smarter than a 5th grader?")
 screen = pygame.display.set_mode([500, 500])
 clock = pygame.time.Clock()
-font = pygame.font.SysFont("comicsansms",50)
+font = pygame.font.SysFont("comicsansms",55)
 
 def intro():
     intro = True
@@ -25,11 +25,11 @@ def intro():
                 pygame.quit()
                 quit()
         screen.fill(white)
-        display_text(screen,"Are you smarter than a 5th grader?",(25,100),font,black)
+        display_text(screen,"Are you smarter than a 5th grader?",(25,25),font,black)
     
        
-        nupufunktsioon("Start",150,300,200,50,darkblue, lightblue,gameloop)
-        nupufunktsioon("End Game", 150,400,200,50,darkblue,lightblue,pygame.quit)
+        nupufunktsioon("Start",50,300,400,80,green, darkgreen,gameloop)
+        nupufunktsioon("End Game", 150,400,200,50,red, darkred,pygame.quit)
     
 
         pygame.display.update()
